0eae8939ae2b0ad2e7d973e71a50002f0c76834e
MarceloxASG <jeffingaleus254@gmail.com>
2024-06-13 12:16:52 -0500

    re organizacion

--- Back-end/myvenv/Scripts/activate.bat
+++ Back-end/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\ProyectoWebAvnz-main\ProyectoWebAvnz\back-end\myvenv
+set VIRTUAL_ENV=C:\Users\jeffi\Desktop\ProyectoWebAvz\ProyectoWebAvnz\back-end\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
