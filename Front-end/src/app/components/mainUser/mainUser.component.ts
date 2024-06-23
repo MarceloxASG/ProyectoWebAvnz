01664760dd2dd362b56ab3cd2be7ae97bf238724
MarceloxASG <jeffingaleus254@gmail.com>
2024-06-23 18:23:34 -0500

    Principal de user sin delete

--- Front-end/src/app/components/mainUser/mainUser.component.ts
+++ Front-end/src/app/components/mainUser/mainUser.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
+import { PostService, Post } from '../../post.service';
+import { PostModalComponent } from '../post-modal/post-modal.component';
 
 @Component({
   selector: 'app-mainUser',
   templateUrl: './mainUser.component.html',
   styleUrl: './mainUser.component.css'
 })
-export class mainUserComponent {
-  
+export class mainUserComponent implements OnInit {
+  posts: Post[] = [];
+  @ViewChild('modalContainer', { read: ViewContainerRef }) modalContainer!: ViewContainerRef;
+
+  constructor(
+    private PostService: PostService,
+    private resolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.PostService.getPosts().subscribe(posts => this.posts = posts);
+  }
+
+  openPostModal(post?: Post): void {
+    this.modalContainer.clear();
+    const factory = this.resolver.resolveComponentFactory(PostModalComponent);
+    const componentRef = this.modalContainer.createComponent(factory);
+    if (post) {
+      componentRef.instance.post = { ...post };
+      componentRef.instance.editMode = true;
+    }
+    componentRef.instance.closeModal = (result: string) => {
+      this.modalContainer.clear();
+      this.loadPosts();
+    };
+  }
 }
